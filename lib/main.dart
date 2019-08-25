7921e9d6fb2c4e52d1324886f7d2941c0c2a75e1
Tonghdj <450570469@qq.com>
2019-08-25 13:17:19 +0800

    successfully upload one block

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ Future<void> main(args) async {
   final String token = doc['UPLOAD_TOKEN'];
   final QiniuUploader qiniuUploader = QiniuUploader();
 
-  final String imagePath = join(current, 'assets/lexus-lx570.jpg');
+  // final String imagePath = join(current, 'assets/lexus-lx570.jpg');
   // await qiniuUploader.formUpload(token, 'lexus-lx570.jpg', imagePath);
 
   final String videoPath = join(current, 'assets/screenrecord.mp4');
-  await qiniuUploader.resumeUpload(token, 'lexus-lx570.jpg', videoPath);
+  await qiniuUploader.resumeUpload(token, 'assets/screenrecord.mp4', videoPath);
 }
--- lib/resume_uploader.dart
+++ lib/resume_uploader.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
 import 'package:mime/mime.dart';
 import 'package:path/path.dart';
@@ -9,6 +11,7 @@ import 'package:uuid/uuid.dart';
 class QiniuUploader {
   final Uuid uuid = Uuid();
   final Dio dio = Dio();
+  final String domain = 'http://upload-z2.qiniup.com';
 
   Future<void> formUpload(String token, String key, String filePath) async {
     final File file = File(filePath);
@@ -32,9 +35,9 @@ class QiniuUploader {
     // formData.add('key', fileName);
     formData.add('key', key);
     formData.add('token', token);
-    final Response response = await dio.post(url, data: formData,
+    final response = await dio.post(url, data: formData,
         onSendProgress: (int current, int total) {
-      print('$current/$total');
+      print('[formUpload] $current/$total');
     });
     print('response => $response');
   }
@@ -57,24 +60,60 @@ class QiniuUploader {
 
     int readLen = 0;
     int bufferLen = 0;
+    int currentBlock = 0;
+    int finishedBlock = 0;
+    List<int> remainedData = [];
+    List<int> readBuffers = [];
     StreamSubscription streamSubscription;
     const int BLOCK_SIZE = 4 * 1024 * 1024; //4MB, never change
     streamSubscription = file.openRead().listen((chunk) async {
       readLen += chunk.length;
       bufferLen += chunk.length;
+      readBuffers.addAll(chunk);
 
-      if (bufferLen >= BLOCK_SIZE || readLen == fileSize) {
+      if (bufferLen >= BLOCK_SIZE - 1 || readLen == fileSize) {
         streamSubscription.pause();
+        final int blockSize = BLOCK_SIZE - remainedData.length;
+        final List<int> buffersData = readBuffers.sublist(0, blockSize);
+        final List<int> postData = List<int>.from(remainedData);
+        postData.addAll(buffersData);
 
-        print('begin wait => ${DateTime.now()}');
-        await Future.delayed(Duration(seconds: 5));
-        print('finish wait => ${DateTime.now()}');
+        // something reset
+        remainedData = readBuffers.sublist(blockSize);
+        bufferLen = bufferLen - BLOCK_SIZE;
+        readBuffers = [];
 
-        streamSubscription.resume();
+        currentBlock += 1;
+
+        await _mkblk(domain, token, postData);
+
+        // streamSubscription.resume();
+        streamSubscription.cancel();
       }
     }, onDone: () {
-      print('finish read file size');
+      print('finish read file size => $bufferLen');
       streamSubscription.cancel();
     });
+  }
+
+  Future<void> _mkblk(String domain, String token, List<int> data) async {
+    try {
+      final String url = '$domain/mkblk/${data.length}';
+      final String auth = 'UpToken $token';
+      final headers = {
+        'Authorization': auth,
+        'Content-Type': 'application/octet-stream',
+        'Connection': 'keep-alive'
+      };
+      // final Response response =
+      //     await dio.post(url, data: data, options: Options(headers: headers));
+      // return response;
+
+      final response = await http.post(url, body: data, headers: headers);
+
+      print(jsonEncode(response.body));
+    } on DioError catch (e) {
+      print(e);
+    }
   }
 }
